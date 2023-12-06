3cd4ff6b103988091217539dc5bbf86d7126bd80
Scott Morrison <scott.morrison@gmail.com>
2023-12-06 17:32:59 +1100

    cleanup with Rob

---
+++ LeanSage/ForMathlib.lean
@@ -0,0 +1,82 @@
+import Mathlib
+
+open Nat
+
+@[simp] theorem ZMod.val_coe (a : ZMod p) : (a.val : ZMod p) = a := sorry
+
+-- This theorem just belongs in Mathlib, and is surely out there!
+theorem IsPrimitiveRoot_iff {p : ℕ} [Fact (p.Prime)] (a : ZMod p) :
+    IsPrimitiveRoot a (p - 1) ↔ a ≠ 0 ∧ ∀ q ∈ primeFactors (p - 1), a ^ ((p - 1) / q) ≠ 1 := by
+  have p_prime : p.Prime := Fact.out
+  constructor
+  · rintro ⟨h1, h2⟩
+    simp
+    constructor
+    · sorry -- easy
+    intro q q_prime dvd
+    specialize h2 ((p-1)/q)
+    intro w
+    intro h
+    specialize h2 h
+    have c0 : 0 < (p - 1) / q := by
+      apply Nat.div_pos
+      · exact le_of_dvd (Nat.sub_pos_iff_lt.mpr (Prime.one_lt p_prime)) dvd
+      · exact Prime.pos q_prime
+    have c1 := le_of_dvd c0 h2
+    have c2 : (p - 1) / q < p - 1 :=
+      div_lt_self (Nat.sub_pos_iff_lt.mpr (Prime.one_lt p_prime)) (Prime.one_lt q_prime)
+    simpa using lt_of_le_of_lt c1 c2
+  · -- We lift `a` in to `(ZMod p)ˣ`.
+    rintro ⟨nz, h⟩
+    have h0 : a * a ^ (p - 2) = 1 := by
+      rw [mul_comm, ← _root_.pow_succ']
+      sorry
+    let au : (ZMod p)ˣ := Units.mkOfMulEqOne a (a^(p-2)) h0
+    revert h
+    rw [show (a : ZMod p) = (au : ZMod p) from rfl]
+    clear_value au
+    clear nz h0 a
+    rename' au => a
+    intro h
+    simp
+    have fermat : a^(p-1) = 1 := by
+      have : Fact (p.Prime) := ⟨p_prime⟩
+      have := ZMod.pow_card_sub_one_eq_one (p := p) (a := a) (by simp)
+      norm_cast at this
+      sorry -- slightly annoying
+    constructor
+    · exact fermat
+    · intro l w
+      by_contra ndvd
+      let l' := Nat.gcd l (p - 1)
+      have h1 : a ^ l' = 1 := by
+        suffices a ^ (l' : ℤ) = 1 by simp_all
+        rw [gcd_eq_gcd_ab l (p - 1), zpow_add, zpow_mul, zpow_coe_nat, w,
+          one_zpow, zpow_mul, zpow_coe_nat, fermat, one_zpow, one_mul]
+      have h2 : l' ∣ p - 1 := Nat.gcd_dvd_right l (p - 1)
+      have h2b : (p - 1) / l' ≠ 1 := by
+        intro r
+        have : l' = p - 1 := by sorry -- easy divisibility stuff
+        have dvd : p - 1 ∣ l := sorry -- "
+        contradiction
+      let q := minFac ((p - 1) / l')
+      have q_prime : q.Prime := minFac_prime h2b
+      have q_dvd : q ∣ (p - 1) / l' := minFac_dvd _
+      have h4 : ∃ c, l' * c = (p - 1) / q := by
+        obtain ⟨c, w⟩ := exists_eq_mul_right_of_dvd q_dvd
+        use c
+        sorry -- yuck!
+      obtain ⟨c, h5⟩ := h4
+      have h6 : a^(l' * c) = 1 := by
+        rw [pow_mul, h1, one_pow]
+      simp at h
+      have : Fact (p.Prime) := ⟨p_prime⟩
+      have h7 : a^((p-1)/q) ≠ 1 := by
+        have := h q q_prime sorry p_prime.one_lt -- a divisibility argument
+        -- Gross, but:
+        norm_cast at this
+        intro y
+        rw [y] at this
+        simp at this
+      rw [h5] at h6
+      exact h7 h6
--- LeanSage/Iff.lean
+++ LeanSage/Iff.lean
@@ -1,58 +1,29 @@
 import Mathlib
+import LeanSage.ForMathlib
 
 open Nat
 
-def sagePrimeFactors (n : ℕ) : List ℕ := sorry
-@[simp] axiom mem_sagePrimeFactors_iff {p n : ℕ} : p ∈ sagePrimeFactors n ↔ p.Prime ∧ p ∣ n
+def sageOutput (args : Array String) : IO String := do
+  IO.Process.run { cmd := "sage", args := args }
 
-def isPrimitiveRoot (a : ℕ) (p : ℕ) : Bool :=
-  (sagePrimeFactors (p - 1)).all fun q => a ^ ((p - 1) / q) % p ≠ 1
+def String.parseNatList (l : String) : List ℕ :=
+  (((l.drop 1).dropRight 2).split (. = ' ')).map
+    (fun s => s.stripSuffix ",") |> .map String.toNat!
 
-theorem isPrimitiveRoot_iff (a : ℕ) (p : ℕ) (prime : p.Prime) :
-    isPrimitiveRoot a p ↔ IsPrimitiveRoot (a : ZMod p) (p - 1) := by
-  constructor
-  · intro h
-    dsimp [isPrimitiveRoot] at h
-    simp at h
-    constructor
-    · have : Fact (p.Prime) := ⟨prime⟩
-      apply ZMod.pow_card_sub_one_eq_one
-      sorry
-    · intro l w
-      by_contra
-      let l' := Nat.gcd l (p - 1)
-      have hp : p - 1 ≠ 0 := by simp [prime.one_lt]
-      have l'nz : l' ≠ 0 := Nat.gcd_ne_zero_right hp
-      have h1 : (a : ZMod p)^l' = 1 := sorry -- by Euclidean algorithm
-      have h2 : l' ∣ p - 1 := Nat.gcd_dvd_right l (p - 1)
-      have hp1l : ¬ ((p - 1) / l' = 0 ∨ (p - 1) / l' = 1)
-      . sorry
-        -- simp [div_ne_zero hp (show l' ≠ _ from l'nz)] -- ??????
-      let q := (factors ((p - 1) / l')).head (by simp [hp1l])
-      have q_prime : q.Prime := prime_of_mem_factors (List.head_mem _)
-      have q_dvd : q ∣ (p - 1) / l' := dvd_of_mem_factors (List.head_mem _)
-      -- have h4 : ∃ c, l' * c = (p - 1) / q := by sorry
-      obtain ⟨c, h5⟩ := q_dvd
-      have h6 : a^(l' * c) = 1 := sorry
-      have h7 : a^((p-1)/q) ≠ 1 := sorry
-      sorry
-  · rintro ⟨h1, h2⟩
-    dsimp [isPrimitiveRoot]
-    simp
-    intro q q_prime dvd
-    specialize h2 ((p-1)/q)
-    intro w
-    specialize h2 ?_
-    · norm_cast
-      convert (ZMod.eq_iff_modEq_nat p (b := 1)).mpr _
-      . simp
-      . show _ % _ = _ % _
-        simp [w, prime.one_lt, Nat.mod_eq_of_lt]
-    have c0 : 0 < (p - 1) / q := by
-      apply Nat.div_pos
-      · exact le_of_dvd (Nat.sub_pos_iff_lt.mpr (Prime.one_lt prime)) dvd
-      · exact Prime.pos q_prime
-    have c1 := le_of_dvd c0 h2
-    have c2 : (p - 1) / q < p - 1 := div_lt_self (Nat.sub_pos_iff_lt.mpr (Prime.one_lt prime)) (Prime.one_lt q_prime)
-    have := lt_of_le_of_lt c1 c2
-    simp at this
+unsafe def sagePrimeFactorsUnsafe (n : ℕ) : List ℕ :=
+  let args := #["-c", s!"print(prime_factors({n}))"] ;
+  match unsafeBaseIO (sageOutput args).toBaseIO with
+  | .ok l => l.parseNatList
+  | .error _ => []
+
+@[implemented_by sagePrimeFactorsUnsafe]
+opaque sagePrimeFactors (n : ℕ) : List ℕ
+
+@[simp] axiom mem_sagePrimeFactors_iff {p n : ℕ} : p ∈ sagePrimeFactors n ↔ p ∈ primeFactors n
+
+def sageIsPrimitiveRoot (a : ℕ) (p : ℕ) : Bool :=
+  (a : ZMod p) != 0 && (sagePrimeFactors (p - 1)).all fun q => (a ^ ((p - 1) / q) : ZMod p) != 1
+
+theorem IsPrimitiveRoot_iff_sageIsPrimitiveRoot {p : ℕ} [Fact (p.Prime)] (a : ZMod p) :
+    IsPrimitiveRoot a (p - 1) ↔ sageIsPrimitiveRoot a.val p := by
+  simp [IsPrimitiveRoot_iff, sageIsPrimitiveRoot]
